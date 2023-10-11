23c551f2f5f901d2e7db061f23323ae8100a88a5
Roman lesyo <ladiginscormag@gmail.com>
2023-10-11 15:18:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,69 +1,68 @@
-import { errorsMidleware } from './src/middlewares/errorsMiddleware.js';
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { Server } from 'socket.io';
-import http from 'http';
-import { dbConnect } from './src/services/dbConnect.js';
+import { errorsMidleware } from './src/middlewares/errorsMiddleware.js'
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { Server } from 'socket.io'
+import http from 'http'
+import { dbConnect } from './src/services/dbConnect.js'
 
 // routes
 import {
-  globalRouter,
-  authRouter,
-  roomsRouter,
-  userRouter,
-  privateChatsRouter,
-  roomsChatRouter,
-} from './src/routes/index.js';
+	globalRouter,
+	authRouter,
+	roomsRouter,
+	userRouter,
+	privateChatsRouter,
+	roomsChatRouter,
+} from './src/routes/index.js'
 
-dotenv.config();
-const app = express();
+dotenv.config()
+const app = express()
 
 // Load environment variables
-const PORT = process.env.SERVER_PORT || 8080;
-const startupDevMode = app.get('env') === 'development';
+const PORT = process.env.SERVER_PORT || 8080
+const startupDevMode = app.get('env') === 'development'
 
-dbConnect();
+dbConnect()
 
 const frontURL = ['http://localhost:3000', 'http://localhost:3001', 'https://our-chat-app-two.vercel.app']
 
 // Set up the express application
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true });
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
 // Обробник опціонального запиту OPTIONS для CORS
-app.options('/auth/signup', cors());
 
 app.use(
-  cors({
-    origin: frontURL,
-    credentials: true,
-    optionsSuccessStatus: 200,
-  }
-));
+	cors({
+		origin: frontURL,
+		credentials: true,
+		optionsSuccessStatus: 200,
+	})
+)
 
-app.use(express.static('public'));
-app.use('/images', express.static('images'));
+app.use(express.static('public'))
+app.use('/images', express.static('images'))
 
 // routes
-app.use('/', globalRouter);
-app.use('/auth', authRouter);
-app.use('/rooms', roomsRouter);
-app.use('/user', userRouter);
+app.use('/', globalRouter)
+app.use('/auth', authRouter)
+app.use('/rooms', roomsRouter)
+app.use('/user', userRouter)
 
 // Necessary to resolve server crash when an error occurs
-// app.use(errorsMidleware);
+// app.use(errorsMidleware)
 
-const httpServer = http.createServer(app);
+const httpServer = http.createServer(app)
 const io = new Server(httpServer, {
-  cors: {
-    origin: frontURL,
-    optionsSuccessStatus: 200,
-  },
-});
+	cors: {
+		origin: frontURL,
+		optionsSuccessStatus: 200,
+	},
+})
 
-privateChatsRouter(io);
-roomsChatRouter(io);
+privateChatsRouter(io)
+roomsChatRouter(io)
 
-httpServer.listen(PORT, () => console.log(`Listening at Port ${PORT} frontURL ${frontURL}`));
+httpServer.listen(PORT, () => console.log(`Listening at Port ${PORT} frontURL ${frontURL}`))
