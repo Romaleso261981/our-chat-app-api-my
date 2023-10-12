34fdefae6dab73e3978c79542581f571b0e9ea5b
Roman lesyo <ladiginscormag@gmail.com>
2023-10-12 17:23:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -30,8 +30,14 @@ dbConnect()
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
-app.use(cors());  
 app.use(express.json());
+app.use(cors({
+	  origin: '*', // Be sure to switch to your production domain
+	  methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
+	  allowedHeaders: ['Content-Type', 'Authorization'],
+	  credentials: true,
+	  optionsSuccessStatus: 200
+	}));  
 
 app.use(express.static('public'))
 app.use('/images', express.static('images'))
