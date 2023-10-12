7f60e50fe210aa69d722070bdf3addecd76665de
Roman lesyo <ladiginscormag@gmail.com>
2023-10-12 17:25:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -27,26 +27,19 @@ const startupDevMode = app.get('env') === 'development'
 dbConnect()
 
 // Set up the express application
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(logger(formatsLogger));
-app.use(express.json());
-app.use(cors({
-	  origin: '*', // Be sure to switch to your production domain
-	  methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
-	  allowedHeaders: ['Content-Type', 'Authorization'],
-	  credentials: true,
-	  optionsSuccessStatus: 200
-	}));  
-
-app.use(express.static('public'))
-app.use('/images', express.static('images'))
-
-// routes
-app.use('/', globalRouter)
-app.use('/auth', authRouter)
-app.use('/rooms', roomsRouter)
-app.use('/user', userRouter)
+app.use(logger(formatsLogger))
+app.use(express.json())
+app.use(
+	cors({
+		origin: '*', // Be sure to switch to your production domain
+		methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+		credentials: true,
+		optionsSuccessStatus: 200,
+	})
+)
 
 // Necessary to resolve server crash when an error occurs
 // app.use(errorsMidleware)
@@ -61,5 +54,22 @@ const io = new Server(httpServer, {
 
 privateChatsRouter(io)
 roomsChatRouter(io)
+
+app.use(express.static('public'))
+app.use('/images', express.static('images'))
+
+// routes
+app.use('/', globalRouter)
+app.use('/auth', authRouter)
+app.use('/rooms', roomsRouter)
+app.use('/user', userRouter)
+
+app.use((req, res) => {
+	res.status(404).json({ message: 'Not found' })
+})
+
+app.use((err, req, res, next) => {
+	res.status(500).json({ message: err.message })
+})
 
 httpServer.listen(PORT, () => console.log(`Listening at Port ${PORT}`))
